0b583792eb96ccaba80a69173f4a7c347ef49935
Giacomo C. <giacomo.corso@gmail.com>
2024-02-09 18:54:31 +0100

    init repo

---
+++ src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
@@ -0,0 +1,22 @@
+package org.factoriaf5.infiltration;
+
+public class AnnalynsInfiltration {
+
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        // Implement method
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        // Implement method
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        // Implement method
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
+            boolean petDogIsPresent) {
+        // Implement method
+    }
+
+}
