f0d29e6ff15ca24df389d855ab536039ff8a4527
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-11-18 12:55:38 +0100

    Añadidos los métodos requeridos

--- src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
+++ src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
@@ -3,20 +3,21 @@ package org.factoriaf5.infiltration;
 public class AnnalynsInfiltration {
 
     public static boolean canFastAttack(boolean knightIsAwake) {
-        // Implement method
+       return !knightIsAwake;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        // Implement method
+       return !knightIsAwake || prisonerIsAwake;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        // Implement method
+        return !archerIsAwake && prisonerIsAwake;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
             boolean petDogIsPresent) {
-        // Implement method
+        return knightIsAwake && !archerIsAwake && prisonerIsAwake && petDogIsPresent;
+        
     }
 
 }
